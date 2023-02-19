b18122ca3bd6ec8f3107badc8b98a290ff4fe7fa
Alan Naranjo <85073731+L-A-M-J-O@users.noreply.github.com>
2023-02-19 11:46:39 +0100

    navbar

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,25 @@
 import React, { useEffect} from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import { FiSettings } from 'react-icons/fi';
-import { TooltipComponent} from '@syncfusion/ej2-react-popups';
 import './App.css';
 
 function App() {
+  const activeMenu: boolean = true;
   return (
     <div>
       <BrowserRouter>
         <div className='flex relative dark:bg-main-dark-bg'>
           <div className='fixed right-4 bottom-4' style={{ zIndex: '1000'}}>
-            <TooltipComponent>
-
-            </TooltipComponent>
           </div>
+         { activeMenu ? (
+          <div className='w-72 fixed sidebar dark:bg-secondary-dark-bg bg-white'>Sidebar</div>
+         ): (
+          <div className='w-0 dark:bg-secondary-dark-bg'>Sidebar w-0</div>
+         )}
+         <div className={`dark:bg-main-bg bg-main-bg min-h-screen w-full ${activeMenu? 'md:ml-72' : 'flex-2'}`}>
+          <div className='fixed md-static bg-main-bg dark:bg-main-dark-bg navbar w-full'>
+            NavBar
+          </div>
+         </div>
         </div>
       </BrowserRouter>
     </div>
